aa65de11a3b9f06b36db07a5298707c1fe7fd280
HassanJoy <mehedihassan41067@gmail.com>
2021-11-24 20:50:38 +0600

    3rd commit

--- FINAL_LAB_PERFORMANCE_2/index.php
+++ FINAL_LAB_PERFORMANCE_2/index.php
@@ -3,7 +3,7 @@
 <html>
     <head>
         <title>Ajax Calculator</title>
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script type="text/Ajax" src="ajax.php"></script>
         <style>
             .but{
                 width: 25px;
@@ -35,6 +35,48 @@
                 font-size: 20px;
             }
         </style>
+        <script>
+            $(document).ready(function(){
+                $('input[type=button]').click(function(){
+                    var num = $(this).val();
+                    var old = $('#display').html();
+                    //this will clear the screen
+                    if( num === 'C' ){
+                        $('#display').html('');
+                        return;
+                    }
+                    if( num === '=' ){
+                        $('#display').html(old);
+                        return;
+                    }
+                    //var str = $('#display').val()+num;
+                    $.ajax({
+                            url:'ajax.php',
+                            type: "POST",
+                            data:{'action':'operation','num':num,'old':old},
+                            success: function(msg){
+                                $('#display').html(msg);
+                            }
+                        }).error(function(){
+                            $('#display').html('Oops! An error occured');}
+                           );
+                });
+                $('#eql').click(function(){
+                    var num = $('#display').html();
+                    var old = $('#display').html();
+                    $.ajax({
+                            url:'ajax.php',
+                            type: "POST",
+                            data:{'action':'equal', 'num':num, 'old':old},
+                            success: function(msg){
+                                $('#display').html(msg);
+                            }
+                        }).error(function(){
+                            $('#display').html('Oops! An error occured');}
+                           );
+                });
+            });
+        </script>
     </head>
     <body>
 <!--        <button id="me">Click Me</button>-->
